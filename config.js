11efb6703727cdb18af2cec7d83f605055008ad9
Ahmed Emad Saad <asy.ahmed@outlook.com>
2026-03-08 04:04:10 +0300

    Replace supabaseKey with supabaseAnonKey

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
+
 window.SITE_CONFIG = {
   supabaseUrl: "https://urhkqtisdludmqcqhhea.supabase.co",
-  supabaseKey: "sb_publishable_KGE1ejvNnKfKlT1xWEbRTg_xyFA6qHD"
+  supabaseAnonKey: "sb_publishable_KGE1ejvNnKfKlT1xWEbRTg_xyFA6qHD"
 };
