13f57ceff4221cf3eb1dd4f97b32700deb7f6fe6
Ahmed Emad Saad <asy.ahmed@outlook.com>
2026-03-12 05:20:45 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  setupLanguageMenu();
+  setupMobileMenu();
+  applyLang(localStorage.getItem("site_lang") || "en");
+  setupTheme();
+  setupReveal();
+  setupTilt();
+  setupHeroImageMotion();
+  loadRemoteContent();
+  bindCvButtons();
+});
 
